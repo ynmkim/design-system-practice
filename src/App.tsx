010af99fd0f7708f79c81ef1a7f312ff277e333b
김윤미 <younmeet@gmail.com>
2024-06-19 18:08:12 +0900

    chore: 디자인 토큰 tailwind config 설정
    font-family, color, font-size, spcaing 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-title-xxl underline text-gray-600 m-spacing-400 font-LINESeedSansKR">
+        Hello world!
+      </h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,154 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        LINESeedSansKR: ['LINESeedSansKR'],
+        LINESeedSansEN: ['LINESeedSansEN'],
+      },
+      fontSize: {
+        xs: [
+          '12px',
+          {
+            lineHeight: '20px',
+            letterSpacing: '0',
+            fontWeight: '400',
+          },
+        ],
+        sm: [
+          '14px',
+          {
+            lineHeight: '21px',
+            letterSpacing: '0',
+            fontWeight: '400',
+          },
+        ],
+        m: [
+          '16px',
+          {
+            lineHeight: '24px',
+            letterSpacing: '0',
+            fontWeight: '400',
+          },
+        ],
+        l: [
+          '18px',
+          {
+            lineHeight: '26px',
+            letterSpacing: '0',
+            fontWeight: '400',
+          },
+        ],
+        xl: [
+          '20px',
+          {
+            lineHeight: '28px',
+            letterSpacing: '0',
+            fontWeight: '400',
+          },
+        ],
+        ['title-xs']: [
+          '12px',
+          {
+            lineHeight: '20px',
+            letterSpacing: '0',
+            fontWeight: '500',
+          },
+        ],
+        ['title-sm']: [
+          '14px',
+          {
+            lineHeight: '21px',
+            letterSpacing: '0',
+            fontWeight: '600',
+          },
+        ],
+        ['title-m']: [
+          '17px',
+          {
+            lineHeight: '28px',
+            letterSpacing: '0.3px',
+            fontWeight: '700',
+          },
+        ],
+        ['title-l']: [
+          '20px',
+          {
+            lineHeight: '28px',
+            letterSpacing: '0',
+            fontWeight: '700',
+          },
+        ],
+        ['title-xl']: [
+          '32px',
+          {
+            lineHeight: '38px',
+            letterSpacing: '-1px',
+            fontWeight: '700',
+          },
+        ],
+        ['title-xxl']: [
+          '48px',
+          {
+            lineHeight: '54px',
+            letterSpacing: '0',
+            fontWeight: '900',
+          },
+        ],
+      },
+      colors: {
+        green: '#06C755',
+        white: '#FFFFFF',
+        'gray-100': '#FCFCFC',
+        'gray-150': '#F5F5F5',
+        'gray-200': '#EFEFEF',
+        'gray-250': '#E8E8E8',
+        'gray-300': '#DFDFDF',
+        'gray-350': '#C8C8C8',
+        'gray-400': '#B7B7B7',
+        'gray-500': '#949494',
+        'gray-600': '#777777',
+        'gray-650': '#616161',
+        'gray-700': '#555555',
+        'gray-750': '#3F3F3F',
+        'gray-770': '#383838',
+        'gray-800': '#303030',
+        'gray-850': '#1F1F1F',
+        'gray-870': '#1A1A1A',
+        'gray-900': '#111111',
+        black: '000000',
+        'blue-400': '#96B2FF',
+        'blue-500': '#638DFF',
+        'blue-600': '#4D73FF',
+        'blue-700': '#2F59CC',
+        'navy-400': '#C8CFDC',
+        'navy-500': '#98A0B3',
+        'navy-600': '#707991',
+        'navy-700': '#464F69',
+        'navy-800': '#323B54',
+        'navy-850': '#262F4A',
+        'navy-900': '#202A43',
+
+        red: '#FF334B',
+      },
+      spacing: {
+        'spacing-50': '2px',
+        'spacing-100': '4px',
+        'spacing-200': '8px',
+        'spacing-300': '12px',
+        'spacing-400': '16px',
+        'spacing-500': '20px',
+        'spacing-600': '24px',
+        'spacing-700': '28px',
+        'spacing-800': '32px',
+        'spacing-900': '36px',
+        'spacing-1000': '40px',
+        'spacing-1100': '44px',
+        'spacing-1200': '48px',
+        'spacing-1300': '52px',
+        'spacing-1400': '56px',
+      },
+    },
   },
   plugins: [],
 };
