8f4f014760de4324a35b6aa9679a18fa64e5102e
김윤미 <younmeet@gmail.com>
2024-06-19 22:49:12 +0900

    refactor: IconButton 컴포넌트에 className props 추가

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,18 +3,20 @@ interface IIconButtonProps {
   iconAlt: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   // (e: React.MouseEvent<HTMLButtonElement>) => void;
+  className: string;
 }
 
 export default function IconButton({
   iconPath,
   iconAlt,
   onClick,
+  className,
 }: IIconButtonProps) {
   return (
     <button
       type="button"
       onClick={onClick}
-      className="flex justify-center items-center w-5 h-5 rounded-full bg-gray-350"
+      className={`flex justify-center items-center w-5 h-5 rounded-full bg-gray-350 ${className}`}
     >
       <img src={iconPath} alt={iconAlt} />
     </button>
