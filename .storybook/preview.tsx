5c81073fedad0e2d871c3a3ea5bc518cdbfc9478
ynmkim <younmeet@gmail.com>
2025-04-07 17:59:53 +0900

    refector: Accessibility 테스트 통과시키기

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import React from 'react';
 import '../src/index.css';
 
 const preview: Preview = {
@@ -10,6 +11,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <main>
+        <Story />
+      </main>
+    ),
+  ],
 };
 
 export default preview;
